ec50e9ac2daee5d10afc0a5d7742d856342ebc7c
jaoualfr <jaouad.khouider@gmail.com>
2021-06-13 00:10:12 -0400

    Update product.js

--- src/product/product.js
+++ src/product/product.js
@@ -1,10 +1,11 @@
 class Product {
-    constructor(id, type, name, version,color) {
+    constructor(id, type, name, version,color1,color2) {
         this.id = id;
         this.type = type;
         this.name = name;
         this.version = version;
-        this.color = color;
+        this.color1 = color1;
+        this.color2 = color2;
     }
 }
 
